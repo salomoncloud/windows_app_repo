3bc9449a99746f8cf4b70c52fa4fe124a3c46a82
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-15 22:06:13 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -2,19 +2,3 @@ resource "azurerm_resource_group" "salomon" {
   name     = "salomon_win_app"
   location = "canada east"
 }
-resource "azurerm_service_plan" "windows_salomon_app" {
-  name                = "windowssalomonapp"
-  resource_group_name = azurerm_resource_group.salomon_win_app.name
-  location            = azurerm_resource_group.salomon_win_app.location
-  os_type             = each.value.os_type
-  sku_name            = each.value.sku_name
-}
-
-resource "azurerm_windows_web_app" "win_salomon" {
-  name                = "windowsalomon"
-  resource_group_name = azurerm_resource_group.salomon_win_app.name
-  location            = azurerm_service_plan.salomon_win_app.location
-  service_plan_id     = each.value.id
-
-  site_config {}
-}
