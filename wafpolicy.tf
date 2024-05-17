c8fe38306c5e037fea8f6bfb44e6c35ef7aad9c3
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-17 19:11:40 -0400

    Update wafpolicy.tf

--- wafpolicy.tf
+++ wafpolicy.tf
@@ -1,3 +1,13 @@
+locals{
+  waf_policy=[for f in fileset("${path.module}/waf", "[^_]*.yaml") : yamldecode(file("${path.module}/waf/${f}"))]
+  azurewafpolicy_list = flatten([
+    for app in local.waf_policy: [
+      for azurewaf in try(app.listofwafpolicy, []) :{
+        name=azurewaf.policyname
+      }
+    ]
+])
+}
 resource "azurerm_web_application_firewall_policy" "my_first_waf" {
   for_each            ={for sp in local.azurewafpolicy_list: "${sp.name}"=>sp }
   name                = each.value.name
