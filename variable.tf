8bddf900db2788066ae78e24131a71582514b003
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-17 19:42:30 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -38,3 +38,27 @@ variable "waf_ip_add" {
 type=list(string)
 default=["192.168.1.0/24", "10.0.0.0/24"]
 }
+variable "rule_uno" {
+type=string
+default="Rule1"
+}
+variable "top_priority" {
+type=number
+default=1
+}
+variable "matchrule" {
+type=string
+default="MatchRule"
+}
+variable "varname" {
+type=string
+default="RemoteAddr"
+}
+variable "blockaction" {
+type=string
+default="Block"
+}
+variable "rule_dos" {
+type=string
+default="Rule2"
+}
