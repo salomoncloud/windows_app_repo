0f1da7dc51769edf92bee32d2b7320df4bd5b832
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-22 18:43:33 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -35,3 +35,74 @@ resource "azurerm_windows_web_app" "win_salomon" {
 
   site_config {}
 }
+variable "prefix" {
+  default = "mcit_vm"
+}
+
+resource "azurerm_virtual_network" "salomon_net" {
+  name                = "salomon_net"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.salomon.location
+  resource_group_name = azurerm_resource_group.salomon.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.salomon.name
+  virtual_network_name = azurerm_virtual_network.salomon_net.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "salomon_nic" {
+  count               = 5
+  name                = "salomon_nic_${count.index + 1}"
+  location            = azurerm_resource_group.salomon.location
+  resource_group_name = azurerm_resource_group.salomon.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  count                 = 5
+  name                  = local.vm_names[count.index]
+  location              = azurerm_resource_group.salomon.location
+  resource_group_name   = azurerm_resource_group.salomon.name
+  network_interface_ids = [azurerm_network_interface.salomon_nic[count.index].id]  # Corrected to match the count index
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "${local.vm_names[count.index]}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = local.vm_names[count.index]
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+locals {
+  vm_names = ["firstvm", "secondvm", "thirdvm", "fourthvm", "fifthvm"]
+}
