72ad5e23e6a689d9109930b720caa135cbece7b6
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-22 19:17:27 -0400

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -1,14 +1,14 @@
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.salomon_net.location
-  resource_group_name = azurerm_resource_group.salomon_net.name
+  location            = azurerm_resource_group.salomon.location
+  resource_group_name = azurerm_resource_group.salomon.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.salomon.name
+  virtual_network_name = azurerm_virtual_network.salomon_net.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
