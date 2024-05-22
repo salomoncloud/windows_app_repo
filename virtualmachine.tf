b9886a7cb646b8e9cbd2b8a28457d6ba422543c5
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-22 19:26:32 -0400

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -1,54 +1,19 @@
 locals {
   vms = yamldecode(file("${path.module}/vm/virtualmachine.yaml"))
 
-  vms_list = flatten([
-    for app in local.virtualmachine.listofvms : [
-      {
-        name     = vms.name
-        size     = Standard_DS1_v2
-      }
-    ]
-  ])
-}
-
-resource "azurerm_virtual_network" "main" {
-  name                = "salomon_net"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.salomon.location
-  resource_group_name = azurerm_resource_group.salomon.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.salomon.name
-  virtual_network_name = azurerm_virtual_network.salomon_net.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "salomon_nic"
-  location            = azurerm_resource_group.salomon.location
-  resource_group_name = azurerm_resource_group.salomon.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
+  vms_list = [for vm in local.vms.listofvms : {
+    name = vm.name
+    size = vm.size
+  }]
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.salomon.location
-  resource_group_name   = azurerm_resource_group.salomon.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  count                = length(local.vms_list)
+  name                 = "${var.prefix}-${local.vms_list[count.index].name}"
+  location             = azurerm_resource_group.salomon.location
+  resource_group_name  = azurerm_resource_group.salomon.name
+  network_interface_ids= [azurerm_network_interface.main.id]
+  vm_size              = local.vms_list[count.index].size
 
   storage_image_reference {
     publisher = "Canonical"
@@ -56,20 +21,24 @@ resource "azurerm_virtual_machine" "main" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "${local.vms_list[count.index].name}-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+
   os_profile {
-    computer_name  = "hostname"
+    computer_name  = local.vms_list[count.index].name
     admin_username = var.admin_username
     admin_password = var.admin_password
   }
+
   os_profile_linux_config {
     disable_password_authentication = false
   }
+
   tags = {
     environment = "staging"
   }
