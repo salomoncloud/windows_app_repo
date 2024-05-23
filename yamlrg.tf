cf7f312c770d1184f6e074b7cce35fae018baf8a
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-22 20:14:46 -0400

    Update yamlrg.tf

--- yamlrg.tf
+++ yamlrg.tf
@@ -1,8 +1,17 @@
 locals {
   rg = yamldecode(file("${path.module}/rg/rg.yaml"))
 
-  resource_group_list = [for rg in local.rg.rg : {
-  name: resource_group.name
-  location: resource_group.location
-  }]
+  resource_group_list = [
+    for rg_entry in local.rg.rg : {
+      name     = rg_entry.name
+      location = rg_entry.location
+    }
+  ]
+}
+
+resource "azurerm_resource_group" "example" {
+  for_each = { for rg in local.resource_group_list : rg.name => rg }
+
+  name     = each.value.name
+  location = each.value.location
 }
