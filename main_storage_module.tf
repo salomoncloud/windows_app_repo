8a21ccc5369dcf4b146637a167feb34be55c193d
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-24 19:09:55 -0400

    Update main_storage_module.tf

--- main_storage_module.tf
+++ main_storage_module.tf
@@ -1,9 +1,9 @@
 module "storage_account" {
-  source = "./module_storage_account/"
+  source = "./module_storage/"
 
-  storage_account_name  = "mystorageaccount"
-  resource_group_name   = "myresourcegroup"
-  location              = "eastus"
+  storage_account_name  = "salomon_storage"
+  resource_group_name   = "salomon"
+  location              = "canada east"
   account_tier          = "Standard"
 }
 
