2aa8f7b62924cb01801630fa4f3177779553e896
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-24 21:21:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -126,3 +126,8 @@ resource "azurerm_lb_rule" "azlb" {
   idle_timeout_in_minutes        = 5
   probe_id                       = element(azurerm_lb_probe.azlb[*].id, count.index)
 }
+locals {
+  lb_name   = var.name != "" ? var.name : format("%s-lb", var.prefix)
+  pip_name  = var.pip_name != "" ? var.pip_name : format("%s-publicIP", var.prefix)
+  subnet_id = try(coalesce(local.data_subnet_id, var.frontend_subnet_id), null)
+}
