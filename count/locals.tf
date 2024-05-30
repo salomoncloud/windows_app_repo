b4d314e5b9f4f6e31b4e946407fb2a27da6bdd9d
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-29 20:29:30 -0400

    Update locals.tf

--- count/locals.tf
+++ count/locals.tf
@@ -12,7 +12,14 @@ variable "vnet_name_two" {
   type = string
   default = "myVNet_forSalomon"
 }
-
+variable "web_subnet_name_two" {
+  type = string
+  default = "web-subnet-two"
+}
+variable "web_subnet_prefixes_two" {
+  type = list(string)
+  default = ["10.0.1.0/24"]
+}
 variable "vnet_address_space_two" {
   type = list(string)
   default = ["10.0.1.0/16"]
