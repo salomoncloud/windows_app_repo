908d41fe214384a2240f1f6cafc836a55948447c
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-29 20:27:19 -0400

    Update locals.tf

--- count/locals.tf
+++ count/locals.tf
@@ -17,3 +17,9 @@ variable "vnet_address_space_two" {
   type = list(string)
   default = ["10.0.1.0/16"]
 }
+resource "azurerm_subnet" "web" {
+  name                 = var.web_subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.web_subnet_prefixes
+}
