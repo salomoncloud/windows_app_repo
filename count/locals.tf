5ba68283f8ca5e004053258620d7022ad000462c
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-29 20:12:31 -0400

    Update locals.tf

--- count/locals.tf
+++ count/locals.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "vnet_rg" {
   name     = var.resource_group_name
   location = var.location
 }
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.vnet_address_space
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.vnet_rg.location
+  resource_group_name = azurerm_resource_group.vnet_rg.name
 }
 variable "vnet_name" {
   type = string
