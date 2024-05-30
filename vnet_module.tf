a1d32095844e583df9c94bcda89be2c741976797
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-29 20:36:12 -0400

    Update vnet_module.tf

--- vnet_module.tf
+++ vnet_module.tf
@@ -4,10 +4,7 @@ module "vnet_salomon" {
   location            = var.location
   admin_username      = var.admin_username
   admin_password      = var.admin_password
-  admin_username_db   = var.db_admin_username
-  admin_password_db   = var.db_admin_password
-  subscription_id     = var.subscription_id
-  client_id           = var.client_id
-  client_secret       = var.client_secret
-  tenant_id           = var.tenant_id
+  db_admin_username   = var.db_admin_username
+  db_admin_password   = var.db_admin_password
 }
+
