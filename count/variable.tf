8924e51fc593ca3cd3bfb1a2415da8f77a366344
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-29 20:35:48 -0400

    Create variable.tf

---
+++ count/variable.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
+
+variable "db_admin_username" {
+  type = string
+}
+
+variable "db_admin_password" {
+  type = string
+}
